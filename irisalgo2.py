3afe29a624b0bf949fd6a804a925b0ff92101122
Edgar Oregel <e_oregel@u.pacific.edu>
2018-05-01 00:57:53 -0700

    Added the main computational function

--- irisalgo2.py
+++ irisalgo2.py
@@ -113,82 +113,106 @@ def divideData(items):
         i += 1
     return divided_elements, thread_names
 
-    
-alfa = 0.005
-epoch = 400
-neuron = [4, 4, 3] 
-weight = [[0 for j in range(neuron[1])] for i in range(neuron[0])]
-weight_2 = [[0 for j in range(neuron[2])] for i in range(neuron[1])]
-bias = [0 for i in range(neuron[1])]
-bias_2 = [0 for i in range(neuron[2])]
-
-start = T.time()
-for i in range(neuron[0]):
-    for j in range(neuron[1]):
-        weight[i][j] = 2 * random.random() - 1
-
-for i in range(neuron[1]):
-    for j in range(neuron[2]):
-        weight_2[i][j] = 2 * random.random() - 1
-
-print("Total Cost:")
-for e in range(epoch):
-    cost_total = 0
-    for idx, x in enumerate(train_X): 
-        h_1 = vec_mat_bias(x, weight, bias)
-        X_1 = sigmoid(h_1)
-        h_2 = vec_mat_bias(X_1, weight_2, bias_2)
-        X_2 = sigmoid(h_2)
-        target = [0, 0, 0]
-        target[int(train_y[idx])] = 1
-        eror = 0
-        for i in range(3):
-            eror +=  0.5 * (target[i] - X_2[i]) ** 2 
-        cost_total += eror
-        delta_2 = []
-        for j in range(neuron[2]):
-            delta_2.append(-1 * (target[j]-X_2[j]) * X_2[j] * (1-X_2[j]))
-
-        for i in range(neuron[1]):
+def computationalFunction():    
+    for e in range(epoch):
+        cost_total = 0
+        for idx, x in enumerate(train_X): 
+            h_1 = vec_mat_bias(x, weight, bias)
+            X_1 = sigmoid(h_1)
+            h_2 = vec_mat_bias(X_1, weight_2, bias_2)
+            X_2 = sigmoid(h_2)
+            target = [0, 0, 0]
+            target[int(train_y[idx])] = 1
+            eror = 0
+            for i in range(3):
+                eror +=  0.5 * (target[i] - X_2[i]) ** 2 
+            cost_total += eror
+            delta_2 = []
             for j in range(neuron[2]):
-                weight_2[i][j] -= alfa * (delta_2[j] * X_1[i])
-                bias_2[j] -= alfa * delta_2[j]
-        delta_1 = mat_vec(weight_2, delta_2)
-        for j in range(neuron[1]):
-            delta_1[j] = delta_1[j] * (X_1[j] * (1-X_1[j]))
-        
-        for i in range(neuron[0]):
+                delta_2.append(-1 * (target[j]-X_2[j]) * X_2[j] * (1-X_2[j]))
+
+            for i in range(neuron[1]):
+                for j in range(neuron[2]):
+                    weight_2[i][j] -= alfa * (delta_2[j] * X_1[i])
+                    bias_2[j] -= alfa * delta_2[j]
+            delta_1 = mat_vec(weight_2, delta_2)
             for j in range(neuron[1]):
-                weight[i][j] -=  alfa * (delta_1[j] * x[i])
-                bias[j] -= alfa * delta_1[j]
-    
-    cost_total /= len(train_X)
-    if(e % 100 == 0):
-        print(cost_total)
+                delta_1[j] = delta_1[j] * (X_1[j] * (1-X_1[j]))
+            
+            for i in range(neuron[0]):
+                for j in range(neuron[1]):
+                    weight[i][j] -=  alfa * (delta_1[j] * x[i])
+                    bias[j] -= alfa * delta_1[j]
+        
+        cost_total /= len(train_X)
+        if(e % 100 == 0):
+            print(cost_total)
 
 
-stop = T.time()
-elapsed = stop - start
-print("\nTotal Time Elapsed:")
-print(str(elapsed) + " sec")
 
-res = matrix_mul_bias(test_X, weight, bias)
-res_2 = matrix_mul_bias(res, weight_2, bias)
+def main():
+    alfa = 0.005
+    epoch = 400
+    neuron = [4, 4, 3] 
+    weight = [[0 for j in range(neuron[1])] for i in range(neuron[0])]
+    weight_2 = [[0 for j in range(neuron[2])] for i in range(neuron[1])]
+    bias = [0 for i in range(neuron[1])]
+    bias_2 = [0 for i in range(neuron[2])]
 
-# Get prediction
-preds = []
-for r in res_2:
-    preds.append(max(enumerate(r), key=lambda x:x[1])[0])
+    start = T.time()
+    for i in range(neuron[0]):
+        for j in range(neuron[1]):
+            weight[i][j] = 2 * random.random() - 1
 
-# Print prediction
-print("\nPredictions:")
-for item in preds:
-    print(item)
+    for i in range(neuron[1]):
+        for j in range(neuron[2]):
+            weight_2[i][j] = 2 * random.random() - 1
 
-# Calculate accuracy
-acc = 0.0
-for i in range(len(preds)):
-    if preds[i] == int(test_y[i]):
-        acc += 1
-print("\nAccuracy:")
-print(str(acc / len(preds) * 100) + "%")
+    elements, thread_names = divideData(epoch);
+
+    thread_processes = []
+
+    first = True
+
+    for thread in thread_names:
+        if first:
+            x = 0
+            y = elements[thread]
+        else:
+            x = elements[thread]
+            y = elements[thread] + elements[thread]
+        thread_start_time = T.time()
+        thread = MP.Process(target=computationalFunction, args=(alfa, epoch, neuron, weight, bias, x, y, thread, thread_start_time, thread_times))
+        thread_processes.append(thread)
+        thread.start()
+
+    stop = T.time()
+    elapsed = stop - start
+    print("\nTotal Time Elapsed:")
+    print(str(elapsed) + " sec")
+
+    res = matrix_mul_bias(test_X, weight, bias)
+    res_2 = matrix_mul_bias(res, weight_2, bias)
+
+    # Get prediction
+    preds = []
+    for r in res_2:
+        preds.append(max(enumerate(r), key=lambda x:x[1])[0])
+
+    # Print prediction
+    print("\nPredictions:")
+    for item in preds:
+        print(item)
+
+    # Calculate accuracy
+    acc = 0.0
+    for i in range(len(preds)):
+        if preds[i] == int(test_y[i]):
+            acc += 1
+    print("\nAccuracy:")
+    print(str(acc / len(preds) * 100) + "%")
+
+
+if __name__ == "__main__":
+    p = MP.Pool(5)
+    main()
